017a50f9c2572f87657b489bc1b7ef36bed92a39
Hasan <hzafar300@outlook.com>
2022-04-29 15:32:13 -0400

    2nd Screen Done - start planning for 5 other screens

--- main.py
+++ main.py
@@ -10,12 +10,10 @@ from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
-
 class SM(ScreenManager):
 
     def __init__(self, **kwargs):
         super(SM, self).__init__(**kwargs)
-
 
 class Screen1(Screen):
 
@@ -36,17 +34,27 @@ class Screen1(Screen):
             greeting.text = "Invalid"
             greeting.color = "#FF0000"
 
-
 class Screen2(Screen):
     def __init__(self, **kwargs):
         super(Screen2, self).__init__(**kwargs)
 
+    def callback(self, instance):
+
+        entry = self.manager.ids['screen2'].ids.my_input.text
+        greeting = self.manager.ids['screen2'].ids.my_label
+
+        # If Input is Valid
+        if entry.isnumeric() and 1 <= int(entry) <= 5:
+            greeting.text = "Calculating"
+            greeting.color = '#FF0000'
+        else:
+            greeting.text = "Invalid"
+            greeting.color = "#FF0000"
 
 class MZ_Invest(App):
     def build(self):
         sm = SM()
         return sm
-
 
 if __name__ == "__main__":
     MZ_Invest().run()
